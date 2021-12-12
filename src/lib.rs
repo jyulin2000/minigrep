7bd95dc50a14ed1ff7536960216aea1c8e442c42
jyulin2000 <jyulin2000@gmail.com>
2021-12-11 21:33:04 -0500

    Added first unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,32 @@
 use std::{ fs, process };
 use std::error::Error;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
     
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
 
 pub fn handle_error(error_string: &str) -> ! {
